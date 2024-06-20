7efa263949ac13b13e018cbe0c24ba111efb626b
Ollie Beenham <ollie@beenhamow.co.uk>
2024-06-20 13:44:52 +0100

    Added latest fetch route

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.get("/test/", (req, res) => res.status(200).json({ message: "Hello World!" }));
 
 app.get("/fetch-latest/", (req, res) => {
-    res.status(200).json({ message: "Fetching latest data" });
+    res.status(200).json({ temperature: 25, humidity: 50, moisture: 75, environmentState: "Good", soilState: "Good", mode: "Auto" });
 });
 
 app.listen(PORT, () => {
