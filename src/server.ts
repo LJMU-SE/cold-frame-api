e9e7d6e2833a6212b77b915bb00183d657891b87
Ollie Beenham <ollie@beenhamow.co.uk>
2024-06-20 13:36:39 +0100

    Added latest fetch route

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.get("/test/", (req, res) => res.status(200).json({ message: "Hello World!" }));
 
+app.get("/fetch-latest/", (req, res) => {
+    res.status(200).json({ message: "Fetching latest data" });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
